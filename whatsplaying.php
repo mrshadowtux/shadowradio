be4f934d4884a22a4b5be72c8ca06bcd4ffb4228
mrshadowtux <alexander.feld@euda.org>
2015-03-21 20:19:06 +0000

    1Live hinzugefügt

--- whatsplaying.php
+++ whatsplaying.php
@@ -5,6 +5,9 @@
 		case "WDR2":
 			system('wget www.wdr2.de/musik/playlist116.html -O - 2>/dev/null | grep currentSongCont -A5 | tail -n2 | sed "s/<strong>//g;s/<\/strong>//g" | xargs');
 		break 1;
+		case "1Live":
+			system('wget www.einslive.de/einslive/musik/playlist/ -O - 2>/dev/null | grep -v form | grep searchPlaylistResult -A 11 | tail -n1 | sed "s/<td><strong>//;s/<\/strong>/:/;s/<\/td>//g"');
+		break 1;
 		default:
 			echo "ToDO - Brauch noch Code zum parsen";
 		break 1;
